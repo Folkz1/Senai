726259d855f26e10809e03e604f80cfcdb637ea6
Diego <diegovilson.1999@gmail.com>
2018-06-14 10:01:24 -0300

    MUdanças
    Erros arrumados

--- Senai.php
+++ Senai.php
@@ -4,9 +4,11 @@ $tamanhoCaminhao = 14*2.6*1.5;//m
 $pesoCaminhao = 6000;//Kg
 $tanque = 150;//L
 $consumoDoCombustivel = 2.5;// K/L
-$quantiaDeKmAteParada = $tanque/$consumoDoCombustivel;
+$quantiaDeKmAteParada = $tanque*$consumoDoCombustivel;
 //echo $quantiaDeKmAteParada;
 //echo '<br>';
+
+
 
 
 //combustivel
@@ -18,6 +20,13 @@ $tamanhoCaixa = 0.6*0.4*0.25;//m
 $pesoCaixa = 0.4;//KG
 $quantidadeDeEmbalagemcaixa = 60;//un
 
+//carregou as 910
+$cabeDentroDocaminhao = $tamanhoCaminhao/$tamanhoCaixa;
+//echo $cabeDentroDocaminhao;//910 caixas
+
+//Distância 
+$distanciaViagemMuranoTopaziaSafirahome = 130; //Km
+$distanciaBrilhanteEsmeraldaHome = 49.8; //Km
 
 //Tempo 
 $abastecimento = 30;//min
@@ -25,37 +34,40 @@ $descarga = 25;//min
 $carregamentoDaCarga = 50;//min
 $descargaCadaEmbalagem = 30;//s
 $intervaloParaDescaco = 60;//min
-$tempoDescargaMurano = 234*30/60;//min
-$tempoDescargaTopazia = 328*30/60;//min
-$tempoDescargaSafira = 25;
-$tempoDescargaEsmeralda = 25;
-$tempoDescargaBrilhantes = 144*30/60;
-$tempoViagemMuranoTopaziaSafirahome = 144;//min 2h 10
-$tempoBrutoDia = ((60*18)+30)-(8*60);
-$descontoDeTempo1 = ($abastecimento*2)+$carregamentoDaCarga+$intervaloParaDescaco+$tempoDescargaTopazia+$tempoDescargaMurano+$tempoViagemMuranoTopaziaSafirahome+$tempoDescargaSafira;
+$tempoDescargaMurano = 234*$descargaCadaEmbalagem/60;//min
+$tempoDescargaTopazia = 328*$descargaCadaEmbalagem/60;//min
+$tempoDescargaSafira = 25;//min
+$tempoDescargaEsmeralda = 25;//min
+$tempoDescargaBrilhantes = 144*30/60;//min
+$tempoViagemMuranoTopaziaSafirahome = 145;//min 2h 10
+$tempoBrutoDia =(18-8)*60+30-60; 
+$descontoDeTempo1 = $carregamentoDaCarga+$intervaloParaDescaco+$tempoDescargaTopazia+$tempoDescargaMurano+$tempoViagemMuranoTopaziaSafirahome+$tempoDescargaSafira;
 $tempoDia1total =  $tempoBrutoDia-$descontoDeTempo1;
+echo "<h1>Informações Principais</h1>";
+echo "<ul>
+		<li>Quantia de caixas que cabem no caminhão: $cabeDentroDocaminhao un</li>
+		<li>Autonomia de kilometros até a parada: $quantiaDeKmAteParada Km</li>
+		<li>Tempo bruto para o trabalho diário: $tempoBrutoDia min</li>
+		<li></li>
+
+	</ul>";
 echo "<h1>Dia1</h1>";
-echo "Tempo sobrando depois de ir há MuranoLTDA,TopázioLTDA e SafiraLTDA: ".$tempoDia1total."min";
-//Total de minutos que sobraram; 2minutos.
-$gasolinaSobrou = (60*3)-130; echo "<br>Gasolina que sobrou dia1: " . $gasolinaSobrou."l";
-echo "<br>Quantia de Km possiveis até acabar a gasolina: ". $gasolinaSobrou/$consumoDoCombustivel. "Km/l";
+echo "Tempo gasto com paradas,descargas e descanço foi de ". ($descontoDeTempo1/60). "h e tempo sobrando depois do percurso ". $tempoDia1total. "min<br>";
+echo "Kilometragem Percorrida foi de $distanciaViagemMuranoTopaziaSafirahome Km, assim, sobrou de autonomia para o veículo ". ($quantiaDeKmAteParada-$distanciaViagemMuranoTopaziaSafirahome). " Km";
 
 echo "<h1>Dia2</h1>";
-$tempoDeViajem = 60+13;//min
-$descontoDeTempo2 = $tempoDeViajem + $tempoDescargaBrilhantes + $tempoDescargaEsmeralda + $abastecimento;
+$tempoBrilhanteEsmeraldaHome = 73;//min
+$descontoDeTempo2 = $tempoBrilhanteEsmeraldaHome + $tempoDescargaBrilhantes + $tempoDescargaEsmeralda;
 $tempoDia2Total = $tempoBrutoDia - $descontoDeTempo2;
-echo "Tempo sobrando Depois de ir na BrilhanteLTDA e EsmeraldaLTDA: $tempoDia2Total Min<br>";
-$gasolinaSobrou2 = ($gasolinaSobrou/$consumoDoCombustivel)+60-49.8;
-echo "Gasolina sobrando: ". $gasolinaSobrou2 ;
-
-//carregou as 910
-$cabeDentroDocaminhao = $tamanhoCaminhao/$tamanhoCaixa;
-
-//echo $cabeDentroDocaminhao;//910 caixas
+echo "Tempo gasto com paradas,descargas e descanço foi de ". ($descontoDeTempo2/60). "h e tempo sobrando depois do percurso ". $tempoDia2Total. "min<br>";
+echo "Kilometragem Percorrida foi de $distanciaBrilhanteEsmeraldaHome Km, assim, sobrou de autonomia para o veículo ". (($quantiaDeKmAteParada-$distanciaViagemMuranoTopaziaSafirahome)-$distanciaBrilhanteEsmeraldaHome). " Km";
 
 
-echo "<h1>Custo:</h1>". "A viajem custará exatamente 3 abastecimento no valor de $precoParaEncherOTanque: R$".$precoParaEncherOTanque*3;
-echo "<p>Isso mais os custos de manutenção do caminhão o pagamento de vale refeição e salário aos empregados e claro os encargos.</p>";
+
+
+
+
+echo "<h1>Custos</h1>". "A viajem custará apenas o salário do motorista e possiveis avarias ao caminhão, já que com o tanque cheio o caminhão fará $quantiaDeKmAteParada . E adicionará os encargos trabalhistas"
 
 
  ?>
